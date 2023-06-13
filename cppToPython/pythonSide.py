d1947c36fefff654bb62b2e7981bef1021f1d88d
Kipper Eyderoe <103725178+Eyderoe@users.noreply.github.com>
2023-06-13 09:41:36 +0800

    Update pythonSide.py

--- cppToPython/pythonSide.py
+++ cppToPython/pythonSide.py
@@ -1,12 +1,14 @@
-# 适用单行
-with open('outputFile.txt', 'r') as fil:
-    nums_str = fil.read().split()
-    nums = [float(num) for num in nums_str]
+def single_line():
+    # 适用单行
+    with open('outputFile.txt', 'r') as fil:
+        nums_str = fil.read().split()
+        nums = [float(num) for num in nums_str]
+    return nums
 
-# 适用多行
-with open('outputFile.txt', 'r') as fil:
-    lines = [line.strip().split() for line in fil]
-    lines_s = [[float(num) for num in line] for line in lines]
 
-print(nums)
-print(lines_s)
+def multi_lines():
+    # 适用多行
+    with open('outputFile.txt', 'r') as fil:
+        lines = [line.strip().split() for line in fil]
+        lines_s = [[float(num) for num in line] for line in lines]
+    return lines_s
