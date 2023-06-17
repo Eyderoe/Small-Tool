1f2f3cddfadaff5f59c249ad64d4f3a3d8610f4e
Kipper Eyderoe <k932693932@Outlook.com>
2023-06-17 12:10:49 +0800

    alfa

--- dataSharing/cppToPython/cppSide.cpp
+++ dataSharing/cppToPython/cppSide.cpp
@@ -2,21 +2,20 @@
 #include <iostream>
 #include <iomanip>
 #include <limits>
+#include <string>
 
 namespace eyderoe {
 
 // 1. arrayOutput(容器[地址或引用]，元素个数，模式[追加或覆盖])
 // 2. linking_list单向链表，只支持写入和[]索引。
+// 3. 输出精度问题未解决hmm
 
 template<typename kipper>
 long long arrayOutput (kipper *array, long long num, bool cover) {
     // cpp不能检查数组元素个数，所以需要传入和返回元素个数，并推荐调用时输出。
     std::ofstream outfile;
     outfile << std::setprecision(std::numeric_limits<long double>::digits10);
-    if (cover)
-        outfile.open("../outputFile.txt", std::ios::out);
-    else
-        outfile.open("../outputFile.txt", std::ios::app);
+    outfile.open("../outputFile.txt", cover ? std::ios::out : std::ios::app);
     for (int i = 0 ; i < num ; ++i)
         outfile << array[i] << " ";
     outfile << std::endl;
@@ -25,14 +24,12 @@ long long arrayOutput (kipper *array, long long num, bool cover) {
 }
 
 template<typename floppy>
-long long arrayOutput (floppy &array, long long num, bool cover) {
-    // cpp不能检查数组元素个数，所以需要传入和返回元素个数，并推荐调用时输出。
+long long arrayOutput (floppy &array, bool cover) {
     std::ofstream outfile;
+    long long num;
     outfile << std::setprecision(std::numeric_limits<long double>::digits10);
-    if (cover)
-        outfile.open("./outputFile.txt", std::ios::out);
-    else
-        outfile.open("./outputFile.txt", std::ios::app);
+    num = array.size();
+    outfile.open("../outputFile.txt", cover ? std::ios::out : std::ios::app);
     for (int i = 0 ; i < num ; ++i)
         outfile << array[i] << " ";
     outfile << std::endl;
@@ -44,18 +41,23 @@ template<typename chip>
 class linking_list // cpp11引入了移动复制函数，所以vector效率不算太差，这个class好像没有太大必要。
 {
     private:
-        struct node
-        {
+        struct node {
             chip data{};
             node *next = nullptr;
         };
+        long long num = 0;
         node *head = nullptr;
         node *tail = nullptr;
     public:
         void add (chip dat);
         ~linking_list ();
         chip operator[] (long long index);
+        long long size ();
 };
+template<typename chip>
+long long linking_list<chip>::size () {
+    return num;
+}
 template<typename chip>
 chip linking_list<chip>::operator[] (long long index) {
     if (index < 0)
@@ -89,6 +91,7 @@ linking_list<chip>::~linking_list () {
 template<typename chip>
 void linking_list<chip>::add (chip dat) {
     node *temp = nullptr;
+    num += 1;
     if (!head)
     {
         head = new node;
